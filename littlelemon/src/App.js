a05b8dfe0cdb59bfeb688d3f68c232ee165117b7
Rita Hu <rzhhu@ucdavis.edu>
2024-07-18 15:15:14 -0700

    Fixed paths

--- littlelemon/src/App.js
+++ littlelemon/src/App.js
@@ -2,17 +2,16 @@ import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Main from './components/Main';
-import {BrowserRouter} from 'react-router-dom';
 
 
 
 function App() {
   return (
-    <BrowserRouter>
+    <>
       <Header/>
       <Main/>
       <Footer/>
-    </BrowserRouter>);
+    </>);
 }
 
 export default App;
--- littlelemon/src/index.js
+++ littlelemon/src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { HashRouter as Router } from 'react-router-dom';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>
 );
 
